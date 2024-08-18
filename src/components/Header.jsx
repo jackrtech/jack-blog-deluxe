45d5faa2d2773a432d7dd8ae652185a348de25fb
Jack Rowsell <jack.rowsell.exmouth@gmail.com>
2024-08-18 15:14:19 +0100

    Navbar toggle feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,44 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./Header.css"
+import "./Header.css";
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <header className="bg-dark text-white p-3">
             <div className="container-fluid">
                 <nav className="navbar navbar-expand-lg navbar-dark">
-                    <div id="navbarNav">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/about" className="nav-link">About</Link>
-                        </li>
-                    </ul>
+                    <button 
+                        className="navbar-toggler d-lg-none" 
+                        type="button"
+                        onClick={toggleMenu}
+                        aria-controls="navbarNav"
+                        aria-expanded={isMenuOpen ? "true" : "false"}
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div 
+                        id="navbarNav" 
+                        className={`collapse navbar-collapse ${isMenuOpen ? "show" : ""}`}
+                    >
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/about" className="nav-link">About</Link>
+                            </li>
+                        </ul>
                     </div>
                 </nav>
             </div>
         </header>
-    )
+    );
 }
